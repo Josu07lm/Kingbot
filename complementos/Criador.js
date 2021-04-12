4c98d0530c313e347a43378ffe8c2d1ae7cccb41
Josu07lm <81869195+Josu07lm@users.noreply.github.com>
2021-04-12 16:21:46 -0500

    Delete complementos directory

--- complementos/Criador.js
+++
@@ -1,9 +0,0 @@
-let  handler  =  function  ( m )  {
-  esto . sendContact ( m . chat ,  '593992390339' ,  'Nurutomo' ,  m )
-}
-manejador . help  =  [ 'propietario' ,  'creador' ]
-manejador . etiquetas  =  [ 'información' ]
-
-manejador . comando  =  / ^ ( propietario | creador ) $ / i
-
-módulo . exportaciones  =  manejador
